2f84135d0c94aef9075c95d4310f03ecb12de367
sczuk-a <49285340+sczuk-a@users.noreply.github.com>
2019-05-20 20:18:16 +0200

    moved

--- js/format.js
+++
@@ -1,8 +0,0 @@
-
-function format(number){
-    let pow = Math.floor(Math.log10(number));
-    if(pow <3) return number.toFixed(2);
-    let xxx = number/Math.pow(10,pow);
-    xxx = xxx.toFixed(2);
-    return xxx + "e" + pow;
-}
